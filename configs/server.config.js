d423584de931f33a1112ec123e4ab40dca66aff5
Adarsh jha <adarshjha91101@gmail.com>
2022-06-11 11:46:53 +0530

    corrected production

--- configs/server.config.js
+++ configs/server.config.js
@@ -1,14 +1,7 @@
-// if(process.env.NODE_ENV!="production"){
-//     require('dotenv').config();
-// }
-
-// module.exports={
-//     PORT: process.env.PORT
-// }
-if(process.env.NODE_ENV != 'production'){
+if(process.env.NODE_ENV !== 'production'){
     require('dotenv').config();
  }
  
  module.exports = {
-     PORT : process.env.PORT
+     PORT : process.env.port
  }
